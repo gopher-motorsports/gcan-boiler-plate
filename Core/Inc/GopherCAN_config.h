bf6b801559877efde3d3338392f57a27e02faa8a
Calvin Molitor <52789184+caljay98@users.noreply.github.com>
2023-01-18 13:06:59 -0600

    Updated to match new GCAN

--- Core/Inc/GopherCAN_config.h
+++ Core/Inc/GopherCAN_config.h
@@ -1,39 +1,20 @@
-// GopherCAN_config.h
-//  This is the config file for the STM32F7 Dev Board running a single CAN bus
-
 #ifndef GOPHERCAN_CONFIG_H
 #define GOPHERCAN_CONFIG_H
 
-#define F0XX 0
-#define F4XX 4
-#define F7XX 7
-
-// Begin Configuration defines. These are to be modified by the module specific developer
-
-//#define TARGET F0XX
-//#define TARGET F7XX
-#define TARGET F4XX
-
-// these are defines that only need to be changed if this module needs to interact with
-// non-GopherCAN busses
-#define FIFO0_RX_CALLBACK service_can_rx_hardware(hcan, CAN_RX_FIFO0);
-#define FIFO1_RX_CALLBACK service_can_rx_hardware(hcan, CAN_RX_FIFO1);
-
-// RX and TX buffer sizes. These values should be chosen by the devs
+// RX and TX buffer sizes (bytes)
 #define RX_BUFFER_SIZE 32
 #define TX_BUFFER_SIZE 32
 
-// Note some initialization is different for multi-bus. Check GopherCAN_router_example.c for details
-//#define MULTI_BUS
+// number of connected CAN buses (max 3)
+#define NUM_OF_BUSSES 1
 
-#ifdef MULTI_BUS
-#define CAN_ROUTER
+// if defined, GCAN tries to retransmit messages on their destination bus
+//#define CAN_ROUTER
 
-// up to 3 busses are supported. That is the most available in the STM32 series
-#define NUM_OF_BUSSES 2
+// if defined, all CAN messages are accepted
+//#define NO_FILTER
+
+// if defined, 11-bit ID data messages are filtered out
+//#define IGNORE_DATA
+
 #endif
-
-#endif
-
-
-// End of GopherCAN_config.h
