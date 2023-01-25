f0411eb2db5acf48fd2619d2fd9a9a6c0f87fd97
Alex <92343744+AlexT2052@users.noreply.github.com>
2023-01-25 17:24:11 -0600

    Fixed tabbing issue lol

--- config_pre_build.py
+++ config_pre_build.py
@@ -17,13 +17,22 @@ import os
 # print(config_file_path)
 
 proj_dir = os.getcwd()
+print(proj_dir)
 main_dir = os.path.dirname(proj_dir)
+print(main_dir)
 dir_name = os.path.basename(proj_dir)
+print(dir_name)
 gcannon_path = main_dir + '\\gophercan-lib\\gophercannon'
+print(gcannon_path)
 car_path = gcannon_path + '\\networks\\go4-22c.yaml'
+print(car_path)
 gsense_path = main_dir + '\\Gopher_Sense'
+print(gsense_path)
 config_file_path = proj_dir + '\\' + dir_name + '_config.yaml'
+print(config_file_path)
 os.chdir(gcannon_path)
+print(os.getcwd())
 os.system('python ' + 'gcan_auto_gen.py' + ' ' + car_path)
 os.chdir(gsense_path)
+print(os.getcwd())
 os.system('python ' + 'sensor_cannon.py' + ' ' + config_file_path)
