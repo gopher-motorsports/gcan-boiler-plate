c30048f7f9750aa66558c912a84bf6f81b34d98b
Alex <92343744+AlexT2052@users.noreply.github.com>
2023-01-25 16:17:38 -0600

    Revert test

--- config_pre_build.py
+++ config_pre_build.py
@@ -1,17 +1,29 @@
 import os
 
+# proj_dir = os.getcwd()
+# main_dir = os.path.dirname(proj_dir)
+# dir_name = os.path.basename(proj_dir)
+# gcannon_path = main_dir + '\\gophercan-lib\\network_autogen'
+# car_path = gcannon_path + '\\configs\\example.yaml'
+# gsense_path = main_dir + '\\Gopher_Sense'
+# config_file_path = proj_dir + '\\' + dir_name + '_config.yaml'
+# os.chdir(gcannon_path)
+# os.system('python ' + 'autogen.py' + ' ' + car_path)
+# os.chdir(gsense_path)
+# os.system('python ' + 'gsense_auto_gen.py' + ' ' + config_file_path)
+# print(gcannon_path)
+# print(car_path)
+# print(gsense_path)
+# print(config_file_path)
+
 proj_dir = os.getcwd()
 main_dir = os.path.dirname(proj_dir)
 dir_name = os.path.basename(proj_dir)
-gcannon_path = main_dir + '\\gophercan-lib\\network_autogen'
-car_path = gcannon_path + '\\configs\\example.yaml'
+gcannon_path = main_dir + '\\gophercan-lib\\gophercannon'
+car_path = gcannon_path + '\\networks\\go4-22c.yaml'
 gsense_path = main_dir + '\\Gopher_Sense'
 config_file_path = proj_dir + '\\' + dir_name + '_config.yaml'
 os.chdir(gcannon_path)
-os.system('python ' + 'autogen.py' + ' ' + car_path)
+os.system('python ' + 'gcan_auto_gen.py' + ' ' + car_path)
 os.chdir(gsense_path)
-os.system('python ' + 'gsense_auto_gen.py' + ' ' + config_file_path)
-print(gcannon_path)
-print(car_path)
-print(gsense_path)
-print(config_file_path)
+os.system('python ' + 'sensor_cannon.py' + ' ' + config_file_path)
