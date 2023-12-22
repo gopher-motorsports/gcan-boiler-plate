051674da7a63a52e9c004e9139ca906689c8e289
alext2052 <alextong2004@gmail.com>
2023-12-21 21:08:45 -0600

    Complete changing from 'python3' to just 'python'

--- config_pre_build.py
+++ config_pre_build.py
@@ -20,9 +20,9 @@ config_file_path = os.path.join(proj_dir,dir_name,'_config.yaml')
 # Enter GopherCAN autogen command
 # ---
 os.chdir(gcannon_path)
-os.system('python3 ' + 'autogen.py' + ' ' + car_path)
+os.system('python ' + 'autogen.py' + ' ' + car_path)
 
 # Enter Gopher Sense autogen command - uncomment below on gcan boiler plate once you reach software startup docs part 2
 # ---
 # os.chdir(gsense_path)
-# os.system('python3 ' + 'gsense_auto_gen.py' + ' ' + config_file_path)
+# os.system('python ' + 'gsense_auto_gen.py' + ' ' + config_file_path)
