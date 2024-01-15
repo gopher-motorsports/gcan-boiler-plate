f3acbd037c8aa8e284b59749ae1ad8d0a9bfe2df
alext2052 <alextong2004@gmail.com>
2024-01-15 12:52:32 -0600

    Fix config pre build for gopher sense

--- config_pre_build.py
+++ config_pre_build.py
@@ -15,7 +15,7 @@ if (len(sys.argv) < 2):
 
 car_path = os.path.join(gcannon_path,'configs',sys.argv[1]+'.yaml')
 gsense_path = os.path.join(main_dir,'Gopher_Sense')
-config_file_path = os.path.join(proj_dir,dir_name,'_config.yaml')
+config_file_path = os.path.join(proj_dir,dir_name + '_config.yaml')
 
 # Enter GopherCAN autogen command
 # ---
